b32cc99c1b02a382960fced8aaa558076782190a
olga <uvbookshelf@gmail.com>
2024-07-29 01:17:17 +0300

    result

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -15,37 +15,91 @@
  * Ответ будет приходить в поле {result}
  */
  import Api from '../tools/api';
+ import * as R from 'ramda'
 
  const api = new Api();
 
- /**
-  * Я – пример, удали меня
-  */
- const wait = time => new Promise(resolve => {
-     setTimeout(resolve, time);
- })
+ const countIsLessTen = (count) => count < 10 
+ const countIsMoreTwo = (count) => count > 2 
+ const valueIsPositive = (count) => count > 0 
+ const stringToFloat = (value) => parseFloat(value)
+
+ const intToString = (value) => value.toString();
+ const countOfSymbols = (str) => str.length;
+ const isCountNumbersLessTen = (N) => countIsLessTen(N.length);
+ const isCountNumbersMoreTwo = (N) => countIsMoreTwo(N.length);
+ const checkPositive = R.pipe(stringToFloat, valueIsPositive);
+ const isValuePositive = (N) => checkPositive(N)
+ const isMatchReg = (N) => N.match(/^[0-9.]{3,9}/g);
+ const getFirstElement = (arr) => R.nth(0, arr);
+ const matchResultAfterRegex = R.pipe(isMatchReg, getFirstElement)
+
+
+ const validateValue = (N) => {
+    const isValueEqualRegex = matchResultAfterRegex(N);
+    const equalWithValue = R.equals(isValueEqualRegex);
+    return R.allPass([isCountNumbersLessTen, isCountNumbersMoreTwo, isValuePositive, equalWithValue])(N)
+ }
+
+const stringToNumber = (N) => parseFloat(N);
+const roundToInt = (N) => Math.round(N);
+const pipeCountOfSymbols = R.pipe(intToString, countOfSymbols);
+//const getApiResult = R.pick('result');
+
+const squaring = (N) => N*N;
+const modThree = (N) => N%3;
 
  const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
-     /**
-      * Я – пример, удали меня
-      */
-     writeLog(value);
 
-     api.get('https://api.tech/numbers/base', {from: 2, to: 10, number: '01011010101'}).then(({result}) => {
-         writeLog(result);
-     });
+    const validationError = () => handleError('ValidationError');
+    const tapWriteLog = R.tap(writeLog);
+    
+    writeLog(value);
 
-     wait(2500).then(() => {
-         writeLog('SecondLog')
+    const getAnimal = (N) => 
+        new Promise (async (res, rej) => {
+            try{
+                await api.get(`https://animals.tech/${N}`,{}).then(({result}) => {
+                return res(result);
+                })
+            }
+            catch(e){
+                handleError(e);
+                rej(e);
+            }
+    });
 
-         return wait(1500);
-     }).then(() => {
-         writeLog('ThirdLog');
+    const getNumberByApi = (N) => 
+        new Promise (async (res, rej) => {
+            try{
+                api.get('https://api.tech/numbers/base', {from: 10, to: 2, number: N}).then(({result}) => {
+                    //console.log(result);
+                    return res(result);
+                })
+            }
+            catch(e){
+                handleError(e);
+                rej(e);
+            }
+        });
 
-         return wait(400);
-     }).then(() => {
-         handleSuccess('Done');
-     });
+    const process = R.pipe(
+        stringToNumber,
+        roundToInt,
+        tapWriteLog,
+        getNumberByApi,
+        R.andThen(tapWriteLog),
+        R.andThen(pipeCountOfSymbols),
+        R.andThen(tapWriteLog),
+        R.andThen(squaring),
+        R.andThen(tapWriteLog),
+        R.andThen(modThree),
+        R.andThen(tapWriteLog),
+        R.andThen(getAnimal),
+        R.andThen(handleSuccess)
+    );
+    const sequence = R.ifElse(validateValue, process, validationError);
+    sequence(value);   
  }
 
 export default processSequence;
--- src/helpers/validators.js
+++ src/helpers/validators.js
@@ -13,38 +13,111 @@
  * Если какие либо функции написаны руками (без использования библиотек) это не является ошибкой
  */
 
+import * as R from 'ramda'
+
+const keysOfObject = (obj) => Object.keys(obj);
+const lengthOfObject = (obj) => obj.length;
+const countOfFilteredObject = R.pipe(keysOfObject, lengthOfObject);
+const filterByFunction = R.pipe(R.filter, countOfFilteredObject);
+const curryFilterByFunction = R.curry(filterByFunction);
+
+const isEqualsTwo = R.equals(2);
+const isEqualsOne = R.equals(1);
+
+const isFigureBlue = R.equals('blue');
+const isFigureRed = R.equals('red');
+const isFigureOrange = R.equals('orange');
+const isFigureGreen = R.equals('green');
+const isFigureWhite = R.equals('white');
+
+const isQuantityMoreThenTwo = (count) => count >= 2
+const isQuantityMoreThenThree = (count) => count >= 3
+
+const equalsSomething = (elem) => R.equals(elem)
+const curryEqualsSomething = R.curry(equalsSomething);
+
+const sumOfRedFigures = (shapes) => curryFilterByFunction(isFigureRed)(shapes);
+const sumOfGreenFigures = (shapes) => curryFilterByFunction(isFigureGreen)(shapes);
+const sumOfOrangeFigures = (shapes) => curryFilterByFunction(isFigureOrange)(shapes);
+const sumOfBlueFigures = (shapes) => curryFilterByFunction(isFigureBlue)(shapes);
+
+const isSumOfRedFiguresMoreThree = R.pipe(sumOfRedFigures, isQuantityMoreThenThree);
+const isSumOfGreenFiguresMoreThree = R.pipe(sumOfGreenFigures, isQuantityMoreThenThree);
+const isSumOfOrangeFiguresMoreThree = R.pipe(sumOfOrangeFigures, isQuantityMoreThenThree);
+const isSumOfBlueFiguresMoreThree = R.pipe(sumOfBlueFigures, isQuantityMoreThenThree);
+
+const isSumOfGreenFiguresIsTwo = R.pipe(sumOfGreenFigures, isEqualsTwo);
+const isSumOfRedFiguresIsOne = R.pipe(sumOfRedFigures, isEqualsOne);
+
+const hasGreenTriangle = R.propEq('triangle', 'green');
+
 // 1. Красная звезда, зеленый квадрат, все остальные белые.
 export const validateFieldN1 = ({star, square, triangle, circle}) => {
-    if (triangle !== 'white' || circle !== 'white') {
-        return false;
-    }
-
-    return star === 'red' && square === 'green';
+    const isRedStarAndGreenSquare =  R.and(isFigureRed(star), isFigureGreen(square));
+    const isTwoFiguresWhite =  R.and(isFigureWhite(triangle), isFigureWhite(circle));
+    return R.and(isRedStarAndGreenSquare, isTwoFiguresWhite)
 };
 
 // 2. Как минимум две фигуры зеленые.
-export const validateFieldN2 = () => false;
+export const validateFieldN2 = (shapes) => {
+    const moreThanTwoFiguresGreen = R.pipe(
+        sumOfGreenFigures,
+        isQuantityMoreThenTwo
+    );
+    return moreThanTwoFiguresGreen(shapes)
+};
 
 // 3. Количество красных фигур равно кол-ву синих.
-export const validateFieldN3 = () => false;
+export const validateFieldN3 = (shapes) => {
+    const countRed = sumOfRedFigures(shapes);
+    const countBlue = sumOfBlueFigures(shapes);
+    return curryEqualsSomething(countRed)(countBlue)
+};
 
 // 4. Синий круг, красная звезда, оранжевый квадрат треугольник любого цвета
-export const validateFieldN4 = () => false;
+export const validateFieldN4 = ({star, square, triangle, circle})  => {
+    const isBlueCircleAndRedStar =  R.and(isFigureBlue(circle), isFigureRed(star));
+    return R.and(isBlueCircleAndRedStar, isFigureOrange(square))
+};
 
 // 5. Три фигуры одного любого цвета кроме белого (четыре фигуры одного цвета – это тоже true).
-export const validateFieldN5 = () => false;
+export const validateFieldN5 = (shapes) => {
+    return R.anyPass([isSumOfRedFiguresMoreThree, 
+        isSumOfGreenFiguresMoreThree, 
+        isSumOfOrangeFiguresMoreThree, 
+        isSumOfBlueFiguresMoreThree
+    ])(shapes);
+};
 
 // 6. Ровно две зеленые фигуры (одна из зелёных – это треугольник), плюс одна красная. Четвёртая оставшаяся любого доступного цвета, но не нарушающая первые два условия
-export const validateFieldN6 = () => false;
+export const validateFieldN6 = (shapes) => {
+    return R.allPass([isSumOfGreenFiguresIsTwo, 
+        isSumOfRedFiguresIsOne, 
+        hasGreenTriangle
+    ])(shapes)
+};
 
 // 7. Все фигуры оранжевые.
-export const validateFieldN7 = () => false;
+export const validateFieldN7 = ({star, square, triangle, circle}) => {
+    return R.all(isFigureOrange)([star, square, triangle, circle])
+}
 
 // 8. Не красная и не белая звезда, остальные – любого цвета.
-export const validateFieldN8 = () => false;
+export const validateFieldN8 = ({star, square, triangle, circle}) => {  
+    return R.anyPass([isFigureBlue, 
+        isFigureOrange, 
+        isFigureGreen
+    ])(star)
+}
 
 // 9. Все фигуры зеленые.
-export const validateFieldN9 = () => false;
+export const validateFieldN9 = ({star, square, triangle, circle}) => {
+    return R.all(isFigureGreen)([star, square, triangle, circle])
+}
 
 // 10. Треугольник и квадрат одного цвета (не белого), остальные – любого цвета
-export const validateFieldN10 = () => false;
+export const validateFieldN10 = ({star, square, triangle, circle}) => {
+    const triangleIsNotWhite = R.pipe(isFigureWhite, R.not)(triangle);
+    const triangleIsEqualSquare = curryEqualsSomething(triangle)(square);
+    return R.and(triangleIsEqualSquare, triangleIsNotWhite);
+}
